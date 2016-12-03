052a66d427a3e8c0018aa5609eaf60545d748fce
Preston Buckley <prestonbuckley@gmail.com>
2016-12-02 18:28:34 -0700

    commit for JS problems

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -3,7 +3,14 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
+function isTyler(name) {
+  if(name === "Tyler"){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
   //Code Here
 
 //////////////////PROBLEM 2////////////////////
@@ -12,7 +19,10 @@ var name = 'Tyler';
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
 
 
-  //Code Here
+function getName() {
+  var name= prompt("What is your name?")
+    return name;
+}  //Code Here
 
 
 //////////////////PROBLEM 3////////////////////
@@ -21,7 +31,10 @@ var name = 'Tyler';
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
-
+function welcome() {
+  var name = getName();
+  alert("Welcome, " + name);
+  }
   //Code Here
 
 
@@ -32,7 +45,7 @@ var name = 'Tyler';
 
 //What is the difference between arguments and parameters?
 
-  //Answer Here
+  //
 
 
 //////////////////PROBLEM 5////////////////////
@@ -52,17 +65,19 @@ var name = 'Tyler';
 
 //Create a function called myName that returns your name
 
-  //Code Here
-  
+  function myName() {
+    return "Preston";
+  }
+
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-  //Code Here
+var newMyName = myName;
 
 //Now alert the result of invoking newMyName
 
-
+alert(newMyName);
 
 //////////////////PROBLEM 7////////////////////
 
@@ -70,10 +85,15 @@ var name = 'Tyler';
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
-  //Code Here
+  function outerFn() {
+    return function() {
+      return "Preston";
+    }
+  }
 
 //Now save the result of invoking outerFn into a variable called innerFn.
-
+var innerFn = outerFn();
   //Code Here
 
 //Now invoke innerFn.
+innerFn();
